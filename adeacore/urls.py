df75fe0cb929075c3d0ca9fb4a8bd2a9e4aace17
Alexandra Ivanova <info@adea-treuhand.ch>
2025-12-08 10:35:12 +0100

    Fix: redirect Import hinzugefügt

--- adeacore/urls.py
+++ adeacore/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
